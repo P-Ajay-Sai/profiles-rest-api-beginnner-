aa8e450c8a2f3d17168ba6b1f4820ed812cf59f1
P-Ajay-Sai <117829349+P-Ajay-Sai@users.noreply.github.com>
2022-12-13 12:39:14 +0530

    Added Viewset to Profile API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.views import Response
-
 from rest_framework import status
+from rest_framework import viewsets
 
 from profiles_api import serializers
 
@@ -22,7 +22,7 @@ class HelloApiView(APIView):
 
 
     def post(self,request):
-        """create a hello message with our name """
+
         serializer = self.serializer_class(data = request.data)
 
         if serializer.is_valid():
@@ -38,7 +38,7 @@ class HelloApiView(APIView):
 
     def put(self,request,pk=None):
         """return updating an object"""
-        return Response({"method":"PUT"})
+        return Response({'method':'PUT'})
 
 
     def patch(self,request,pk=None):
@@ -49,3 +49,52 @@ class HelloApiView(APIView):
     def delete(self,request,pk=None):
         """delete an object"""
         return Response({'method':'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSets"""
+
+    serializer_class=serializers.HelloSerializer
+
+    def list(self,request):
+        """return a hello message"""
+        a_viewset=[
+        'users actions (list ,create ,retrive ,update ,partial_update)',
+        'Automatically maps to urls using Routers',
+        "provides more functionality with less code"
+        ]
+        return Response({'message':"hello!",'a_viewset':a_viewset})
+
+    def create(self, request):
+        """Create a new hello message."""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}!'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrieve(self, request, pk=None):
+        """Handle getting an object by its ID"""
+
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        """Handle updating an object"""
+
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """Handle updating part of an object"""
+
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """Handle removing an object"""
+
+        return Response({'http_method': 'DELETE'})
